79053a6242f66bbea8239c18828122ab89345487
Dana Scheider <dana@thinkplango.com>
2013-10-15 14:52:39 -0700

    Added tests for find_outliers and trim methods

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -17,5 +17,17 @@ class Calculator
       results[:upper_fence].should == 36.0
       results[:lower_fence].should == -21.0
     end
+
+    it 'identifies outliers' do
+      calc = Calculator.new
+      results = calc.find_outliers([66, 40, 78, 22, 48, 78, 24, 24, 25, 99, 74, 98, 97, 42, 61, 42, 78, 68, 56, 47, -32, -15, 247, 861])
+      results.sort.should == [-32, -15, 247, 861]
+    end
+
+    it 'removes outliers from a data set' do
+      calc = Calculator.new
+      results = calc.trim([66, 40, 78, 22, 48, 78, 24, 24, 25, 99, 74, 98, 97, 42, 61, 42, 78, 68, 56, 47, -32, -15, 247, 861])
+      results.sort.should == [24, 25, 40, 42, 42, 47, 48, 56, 61, 66, 68, 74, 78, 78, 78, 97, 98, 99]
+    end
   end
 end
