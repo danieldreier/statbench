17ec50c8d07ed3b3ed2627cc8df43e4bce033091
Dana Scheider <dana@thinkplango.com>
2013-10-15 15:22:12 -0700

    There are 2 files for the calculator class. From now on we are using calculator_class.rb, NOT calculator.rb. But I made changes in this one so they should be equivalent now.

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -15,4 +15,29 @@ class Calculator
 		@summary_stats[:lower_fence] = @data.median() - @summary_stats[:iqr] * 1.5
 		@summary_stats
 	end
+
+	# Return the outliers in a data set. Multiplier is the multiple of the IQR above or below the median above/below which a data point is
+	# considered an outlier. We will want to update this method to include other methods of determining outliers.
+	def find_outliers(array, multiplier = 1.5)
+		@data = array.to_vector(:scale)
+		@outliers = Array.new()
+		@data.each do |data_point|
+			if (data_point < @data.median() - (@data.percentil(75) - @data.percentil(25))*multiplier) || (data_point > @data.median() + (@data.percentil(75) - @data.percentil(25))*1.5)
+				@outliers << data_point
+			end
+		end
+		@outliers
+	end
+
+	# Return a data set with outliers removed (using the upper/lower fence method of identifying outliers). Multiplier is the multiple of IQR
+	# used to determine the fences. We will want to update this method to include other methods of returning outliers.
+	def trim(array, multiplier = 1.5)
+		@data = array.to_vector(:scale)
+		@data.each do |data_point|
+			if (data_point < @data.median() - (@data.percentil(75) - @data.percentil(25))*multiplier) || (data_point > @data.median() + (@data.percentil(75) - @data.percentil(25))*multiplier)
+				@data.delete(data_point)
+			end
+		end
+		@data
+	end
 end
