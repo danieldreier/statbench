30e31764bbea7550d3de10f3b7689949d1a6259f
Dana Scheider <dana@thinkplango.com>
2013-11-16 14:28:46 -0800

    Added requirement for data file and removed data set constant definitions Changed names of data sets to those used in the data file

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,27 +1,14 @@
 require 'spec_helper'
+require_relative '../test/data'
 
 # Run me with:
 # rspec spec/calculator_spec.rb --format doc --color
 
 class Calculator
   describe Calculator do
-  SAMPLE_DATASET=[ 66, 40, 78, 22, 48, 78, 24, 24, 25, 99, 74, 98, 97, 42, 61, 
-                   42, 78, 68, 56, 47, -32, -15, 247, 861 ]
-  LARGE_DATASET = [-68, -25, -98, -97, -71, 34, -32, -24, 83, -83, 81, 42,
-                    80, 53, 4, -60, 76, 31, -32, 87, -86, 100, 70, 15, -22, 
-                    40, 18, 13, 48, 5, 41, -97, 37, -39, 48, 21, 98, 78, 
-                    93, 14, 98, 20, 8, -63, -56, -99, 36, 66, -21, 0, -4,
-                    96, 19, -67, -69, -20, 92, 64, 46, -1, 13, 77, -95, 62,
-                   -85, -91, 67, 86, 12, -15, -99, 16, -73, 66, 63 ]
-  SMALL_BINOMIAL_DATASET = [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0 ]
-  LARGE_BINOMIAL_DATASET = [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
-                             1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                             1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,
-                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
     it 'calculates summary statistics' do 
       calc = Calculator.new
-      results = calc.summary_stats(SAMPLE_DATASET)
+      results = calc.summary_stats(SMALL_DATASET_1)
       results[:iqr].should == 45.5
       results[:minimum].should == -32
       results[:maximum].should == 861
@@ -34,13 +21,13 @@ class Calculator
 
     it 'identifies outliers' do
       calc = Calculator.new
-      results = calc.find_outliers(SAMPLE_DATASET)
+      results = calc.find_outliers(SMALL_DATASET_1)
       results.sort.should == [-32, -15, 247, 861]
     end
 
     it 'removes outliers from a data set' do
       calc = Calculator.new
-      results = calc.trim(SAMPLE_DATASET)
+      results = calc.trim(SMALL_DATASET_1)
       results.sort.should == [22, 24, 24, 25, 40, 42, 42, 47, 48, 56, 61, 66, 
                               68, 74, 78, 78, 78, 97, 98, 99]
     end
@@ -52,7 +39,7 @@ class Calculator
     it 'generates a confidence interval for mean of a single population 
     meeting z-test criteria (no population size given)' do 
       calc = Calculator.new 
-      results = calc.mean_confidence_interval({ :data => LARGE_DATASET, 
+      results = calc.mean_confidence_interval({ :data => LARGE_DATASET_1, 
                                            :confidence_level => 0.95, 
                                            :sigma => 55, 
                                            :normal => true 
@@ -65,7 +52,7 @@ class Calculator
     it 'generates a confidence interval for mean of a single population 
     meeting z-test criteria (population size given)' do 
       calc = Calculator.new 
-      results = calc.mean_confidence_interval({ :data => LARGE_DATASET,
+      results = calc.mean_confidence_interval({ :data => LARGE_DATASET_1,
                                                 :confidence_level => 0.95,
                                                 :sigma => 55,
                                                 :population_size => 1000 
@@ -80,7 +67,7 @@ class Calculator
     it 'generates a confidence interval for mean of a single population using 
     a t-test (no population size given)' do
       calc = Calculator.new
-      results = calc.mean_confidence_interval({ :data => LARGE_DATASET })
+      results = calc.mean_confidence_interval({ :data => LARGE_DATASET_1 })
       results[:lower].should be_within(0.01).of(-5.9)
       results[:upper].should be_within(0.01).of(22.57)
       results[:confidence_level].should == 0.95
@@ -89,7 +76,7 @@ class Calculator
     it 'generates a confidence interval for mean of a single population
     using a t-test (population size given)' do 
       calc = Calculator.new
-      results = calc.mean_confidence_interval({ :data => LARGE_DATASET,
+      results = calc.mean_confidence_interval({ :data => LARGE_DATASET_1,
                                                 :population_size => 1000
                                               })
       results[:lower].should be_within(0.01).of(-5.36)
@@ -101,7 +88,7 @@ class Calculator
     it 'generates a confidence interval for mean of a single population using 
     a t-test, sigma known, other z criteria not met (no population size)' do 
       calc = Calculator.new
-      results = calc.mean_confidence_interval({ :data => SAMPLE_DATASET, 
+      results = calc.mean_confidence_interval({ :data => SMALL_DATASET_1, 
                                            :confidence_level => 0.95, 
                                            :sigma => 55 
                                         })
@@ -113,7 +100,7 @@ class Calculator
     it 'generates a confidence interval for mean of a single population using
     a t-test with sigma known (population size given)' do 
       calc = Calculator.new 
-      results = calc.mean_confidence_interval({ :data => SAMPLE_DATASET,
+      results = calc.mean_confidence_interval({ :data => SMALL_DATASET_1,
                                            :confidence_level => 0.95,
                                            :sigma => 55,
                                            :population_size => 1000
@@ -128,7 +115,7 @@ class Calculator
     it 'suggests a sample size for a mean confidence interval given a margin
     of error (z-test)' do 
       calc = Calculator.new 
-      calc.suggest_sample_size({ :data => LARGE_DATASET, 
+      calc.suggest_sample_size({ :data => LARGE_DATASET_1, 
                                  :parameter => 'mean', 
                                  :confidence_level => 0.95, 
                                  :margin_of_error => 20, 
@@ -139,7 +126,7 @@ class Calculator
     it 'suggests a sample size for a mean confidence interval given a 
     margin of error (t-test)' do 
       calc = Calculator.new 
-      calc.suggest_sample_size({:data => SAMPLE_DATASET, 
+      calc.suggest_sample_size({:data => SMALL_DATASET_1, 
                                 :parameter => 'mean', 
                                 :confidence_level => 0.95, 
                                 :margin_of_error => 97
@@ -151,7 +138,7 @@ class Calculator
     it 'generates a confidence interval for proportion of a single population 
     using a large sample and z-test (no population size given)' do
       calc = Calculator.new 
-      results = calc.proportion_confidence_interval(LARGE_BINOMIAL_DATASET)
+      results = calc.proportion_confidence_interval(LARGE_BINOMIAL_DATASET_1)
       results[:lower].should be_within(0.0001).of(0.3935)
       results[:upper].should be_within(0.0001).of(0.6198)
       results[:confidence_level].should == 0.95
@@ -160,7 +147,7 @@ class Calculator
     it 'generates a confidence interval for proportion of a single population using 
     a large sample and z-test (population size given)' do 
       calc = Calculator.new
-      results = calc.proportion_confidence_interval(LARGE_BINOMIAL_DATASET,0.99,1000)
+      results = calc.proportion_confidence_interval(LARGE_BINOMIAL_DATASET_1,0.99,1000)
       results[:lower].should be_within(0.0001).of(0.3560)
       results[:upper].should be_within(0.0001).of(0.6573)
       results[:confidence_level].should == 0.99
@@ -169,7 +156,7 @@ class Calculator
     it 'generates a Wilson score interval for proportion of a single 
     population and small sample' do 
       calc = Calculator.new
-      results = calc.proportion_confidence_interval(SMALL_BINOMIAL_DATASET)
+      results = calc.proportion_confidence_interval(SMALL_BINOMIAL_DATASET_1)
       results[:lower].should be_within(0.0001).of(0.3575)
       results[:upper].should be_within(0.0001).of(0.8018) 
       results[:confidence_level].should == 0.95
@@ -178,7 +165,7 @@ class Calculator
     it 'suggests a sample size for a proportion confidence interval given 
     a margin of error (z-test)' do 
       calc = Calculator.new 
-      calc.suggest_sample_size_proportion({ :data => LARGE_BINOMIAL_DATASET,  
+      calc.suggest_sample_size_proportion({ :data => LARGE_BINOMIAL_DATASET_1,  
                                             :confidence_level => 0.95, 
                                             :margin_of_error => 0.1
                                          }).should be_within(2).of(384) 
@@ -187,7 +174,7 @@ class Calculator
     it 'suggests a sample size for a proportion confidence interval given
     a margin of error (Wilson scores)' do 
       calc = Calculator.new 
-      calc.suggest_sample_size_proportion({ :data => SAMPLE_DATASET, 
+      calc.suggest_sample_size_proportion({ :data => SMALL_DATASET_1, 
                                             :confidence_level => 0.95, 
                                             :margin_of_error => 0.3
                                          }).should be_within(2).of(146) 
@@ -198,7 +185,7 @@ class Calculator
     it 'generates a confidence interval for standard deviation of a single 
     population and small sample' do 
       calc = Calculator.new 
-      results = calc.sdev_confidence_interval(SAMPLE_DATASET, 0.9)
+      results = calc.sdev_confidence_interval(SMALL_DATASET_1, 0.9)
       results[:lower].should be_within(0.01).of(138.74)
       results[:upper].should be_within(0.01).of(227.42) 
       results[:confidence_level].should == 0.9
@@ -207,7 +194,7 @@ class Calculator
     it 'generates a confidence interval for standard deviation of a single 
     population and large sample' do 
       calc = Calculator.new 
-      results = calc.sdev_confidence_interval(LARGE_DATASET,0.95)
+      results = calc.sdev_confidence_interval(LARGE_DATASET_1,0.95)
       results[:lower].should be_within(0.01).of(53.30) 
       results[:upper].should be_within(0.01).of(73.73)
       results[:confidence_level].should == 0.95
@@ -216,7 +203,7 @@ class Calculator
     it 'suggests a sample size for a standard deviation confidence interval
     given a margin of error' do 
       calc = Calculator.new 
-      calc.suggest_sample_size_sdev( { :data => LARGE_DATASET, 
+      calc.suggest_sample_size_sdev( { :data => LARGE_DATASET_1, 
                                        :confidence_level => 0.95, 
                                        :margin_of_error => 17.5495 
                                    } ).should be_within(2).of(100)
