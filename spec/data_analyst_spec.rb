6a23dd4592579ea169cc0fc21f008cb96ddb5265
Dana Scheider <dana@thinkplango.com>
2013-12-12 15:46:53 -0800

    Deleted tests that tested internal functionality; replaced with outcome-driven tests pertaining to differences in mean response times

--- spec/data_analyst_spec.rb
+++ spec/data_analyst_spec.rb
@@ -2,10 +2,24 @@ require_relative 'spec_helper'
 require_relative '../lib/data_analyst.rb'
 
 describe DataAnalyst do 
-  describe 'arguments' do
-    it 'turns array into a vector' do 
-      processor = DataAnalyst.new([1,2,3])
-      expect(processor.data.class).to eql(Statsample::Vector)
+  describe 'mean methods' do
+    describe '.equal_response_time?' do 
+
+      it 'returns false when response time not equal' do 
+        processor = DataAnalyst.new(DATASET_1,DATASET_3)
+        expect(processor.equal_response_time?).to be_false
+      end
+
+      it 'returns true when response time is equal' do 
+        processor = DataAnalyst.new(DATASET_1,DATASET_1)
+        expect(processor.equal_response_time?).to be_true
+      end
+
+    end # '.equal_response_time?'
+
+    it 'calculates difference between two means' do 
+      processor = DataAnalyst.new(DATASET_1,DATASET_2)
+      expect(processor.mean_difference).to be_within(0.005).of(-0.6096)
     end
-  end
+  end # 'mean methods'
 end
