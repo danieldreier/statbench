e162f65578fae220cc903661321825dd2ceb1e60
Dana Scheider <dana@thinkplango.com>
2013-12-12 15:36:46 -0800

    Updated syntax changing deprecated should method to expect

--- spec/data_analyst_spec.rb
+++ spec/data_analyst_spec.rb
@@ -2,10 +2,10 @@ require_relative 'spec_helper'
 require_relative '../lib/data_analyst.rb'
 
 describe DataAnalyst do 
-  describe 'arguments' do 
+  describe 'arguments' do
     it 'turns array into a vector' do 
       processor = DataAnalyst.new([1,2,3])
-      processor.data.class.should eql(Statsample::Vector)
+      expect(processor.data.class).to eql(Statsample::Vector)
     end
   end
 end
