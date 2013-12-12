a959ff2e6e0aef774f6d7f6a810a701075044b9e
Dana Scheider <dana@thinkplango.com>
2013-12-12 15:12:34 -0800

    Changed requirement in spec_helper to require data_analyst.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,1 @@
-require_relative '../lib/calculator'
-require_relative '../lib/confidence_interval'
-require_relative '../lib/critical_value'
-require_relative '../lib/test_statistic'
+require_relative '../lib/data_analyst.rb'
