e17391fb44fb615847e47aa82d4386eafcfdbf12
Dana Scheider <dana@thinkplango.com>
2013-12-12 15:20:43 -0800

    Deleted test "takes array as argument" and replaced with 'turns array into vector'

--- spec/data_analyst_spec.rb
+++ spec/data_analyst_spec.rb
@@ -3,9 +3,9 @@ require_relative '../lib/data_analyst.rb'
 
 describe DataAnalyst do 
   describe 'arguments' do 
-    it 'takes an array as an argument' do 
+    it 'turns array into a vector' do 
       processor = DataAnalyst.new([1,2,3])
-      processor.array.should eql([1,2,3])
+      processor.data.class.should eql(Statsample::Vector)
     end
   end
 end
