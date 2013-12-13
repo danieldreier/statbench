3860736be8ef8b785233ac8ae86683b9166d904a
Dana Scheider <dana@thinkplango.com>
2013-12-12 16:45:26 -0800

    Added tests for #equal_variability? method

--- spec/hypothesis_test_spec.rb
+++ spec/hypothesis_test_spec.rb
@@ -14,5 +14,17 @@ describe HypothesisTest do
         expect(processor.equal_response_time?).to be_true
       end
     end # '#equal_response_time?'
+
+    describe '#equal_variability?' do 
+      it 'returns false when variability is unequal' do 
+        processor = DataAnalyst.new(DATASET_1,DATASET_2)
+        expect(processor.equal_variability?).to be_false
+      end
+
+      it 'returns true when variability is equal' do 
+        processor = DataAnalyst.new(DATASET_1,DATASET_1) 
+        expect(processor.equal_variability?).to be_true
+      end
+    end # '#equal_variability?'
   end # 'testing two means'
 end # 'describe HypothesisTest'
