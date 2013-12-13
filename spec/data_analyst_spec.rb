6093cc64d61c0706a42be1f16a555922853003f5
Dana Scheider <dana@thinkplango.com>
2013-12-12 16:04:14 -0800

    Added tests for methods pertaining to differences in the variability of two data sets

--- spec/data_analyst_spec.rb
+++ spec/data_analyst_spec.rb
@@ -22,4 +22,23 @@ describe DataAnalyst do
       expect(processor.mean_difference).to be_within(0.005).of(-0.6096)
     end
   end # 'mean methods'
+
+  describe 'variability methods' do 
+    describe '.equal_variability?' do 
+      it 'returns false when variability is not equal' do 
+        processor = DataAnalyst.new(DATASET_1,DATASET_3)
+        expect(processor.equal_variability?).to be_false
+      end
+
+      it 'returns true when variability is equal' do 
+        processor = DataAnalyst.new(DATASET_1,DATASET_1)
+        expect(processor.equal_variability?).to be_true
+      end
+    end # '.equal_variability?'
+
+    it 'calculates the difference between two standard deviations' do 
+      processor = DataAnalyst.new(DATASET_1,DATASET_2)
+      expect(processor.sdev_difference).to be_within(0.005).of(-1.0287)
+    end
+  end # 'variability methods'
 end
