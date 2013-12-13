6df796af7d8c5918ead06605f1b5a6d7f359b51f
Dana Scheider <dana@thinkplango.com>
2013-12-12 17:05:01 -0800

    Deleted tests for methods that had been extracted

--- spec/data_analyst_spec.rb
+++ spec/data_analyst_spec.rb
@@ -2,17 +2,5 @@ require_relative 'spec_helper'
 require_relative '../lib/data_analyst.rb'
 
 describe DataAnalyst do 
-  describe 'mean methods' do
-    it 'calculates difference between two means' do 
-      processor = DataAnalyst.new(DATASET_1,DATASET_2)
-      expect(processor.mean_difference).to be_within(0.005).of(-0.6096)
-    end
-  end # 'mean methods'
-
-  describe 'variability methods' do 
-    it 'calculates the difference between two standard deviations' do 
-      processor = DataAnalyst.new(DATASET_1,DATASET_2)
-      expect(processor.sdev_difference).to be_within(0.005).of(-1.0287)
-    end
-  end # 'variability methods'
+  # no methods here, boss!
 end
