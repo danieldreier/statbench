aaa21a3000db535c9113ab24ccaa4c24515a27ee
Dana Scheider <dana@thinkplango.com>
2013-12-12 17:03:57 -0800

    Added method estimating the difference between two standard deviations

--- lib/confidence_interval.rb
+++ lib/confidence_interval.rb
@@ -6,4 +6,8 @@ module ConfidenceInterval
   def mean_difference(data1=@dataset_1,data2=@dataset_2)
     data1.mean - data2.mean
   end
+
+  def sdev_difference(data1=@dataset_1,data2=@dataset_2)
+    data1.standard_deviation_sample - data2.standard_deviation_sample
+  end
 end
