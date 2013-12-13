e30d0904ad3dede52d14bc0a3f6ecbea831a5819
Dana Scheider <dana@thinkplango.com>
2013-12-12 16:47:11 -0800

    Removed tests for #equal_variability? method, which were moved to the HypothesisTest module

--- spec/data_analyst_spec.rb
+++ spec/data_analyst_spec.rb
@@ -10,18 +10,6 @@ describe DataAnalyst do
   end # 'mean methods'
 
   describe 'variability methods' do 
-    describe '.equal_variability?' do 
-      it 'returns false when variability is not equal' do 
-        processor = DataAnalyst.new(DATASET_1,DATASET_3)
-        expect(processor.equal_variability?).to be_false
-      end
-
-      it 'returns true when variability is equal' do 
-        processor = DataAnalyst.new(DATASET_1,DATASET_1)
-        expect(processor.equal_variability?).to be_true
-      end
-    end # '.equal_variability?'
-
     it 'calculates the difference between two standard deviations' do 
       processor = DataAnalyst.new(DATASET_1,DATASET_2)
       expect(processor.sdev_difference).to be_within(0.005).of(-1.0287)
