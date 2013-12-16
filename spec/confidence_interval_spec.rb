3cc3edad2ce22a874ce72b403686cec59f146e26
Dana Scheider <dana@thinkplango.com>
2013-12-16 14:22:09 -0800

    Added tests to make sure 95% confidence level is used by default

--- spec/confidence_interval_spec.rb
+++ spec/confidence_interval_spec.rb
@@ -6,12 +6,22 @@ describe ConfidenceInterval do
       processor = DataAnalyst.new(DATASET_1,DATASET_2)
       expect(processor.mean_difference).to eql({ :interval => [-3.00515, 1.78595], :confidence => 0.95})
     end
+
+    it 'uses 95% confidence level by default' do 
+      processor = DataAnalyst.new(DATASET_1,DATASET_2)
+      expect(processor.mean_difference).to eql(processor.mean_difference(0.95))
+    end
   end # 'mean intervals'
 
   describe 'variability intervals' do 
     it 'gives interval estimate of difference between two standard deviations' do 
       processor = DataAnalyst.new(DATASET_1,DATASET_2)
       expect(processor.sdev_difference).to eql({ :interval => [0.81951, 1.0829], :confidence => 0.95 })
+    end
+
+    it 'uses 95% confidence level by default' do 
+      processor = DataAnalyst.new(DATASET_1,DATASET_2)
+      expect(processor.sdev_difference).to eql(processor.sdev_difference(0.95))
     end
   end # 'variability intervals'
 end # describe ConfidenceInterval
