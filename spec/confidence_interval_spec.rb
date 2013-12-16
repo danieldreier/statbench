fefbaa6c28f6ef1d933a41b7c71efec629e89f43
Dana Scheider <dana@thinkplango.com>
2013-12-16 14:59:49 -0800

    Added before(:all) block to replace processor=DataAnalyst.new(DATASET_1,DATASET_2) in each test

--- spec/confidence_interval_spec.rb
+++ spec/confidence_interval_spec.rb
@@ -1,27 +1,27 @@
 require_relative 'spec_helper'
 
 describe ConfidenceInterval do 
+  before(:all) do 
+    @processor = DataAnalyst.new(DATASET_1,DATASET_2)
+  end
+
   describe 'mean intervals' do 
     it 'gives interval estimate of difference between two means' do 
-      processor = DataAnalyst.new(DATASET_1,DATASET_2)
-      expect(processor.mean_difference).to eql({ :interval => [-3.00515, 1.78595], :confidence => 0.95})
+      expect(@processor.mean_difference).to eql({ :interval => [-3.00515, 1.78595], :confidence => 0.95})
     end
 
     it 'uses 95% confidence level by default' do 
-      processor = DataAnalyst.new(DATASET_1,DATASET_2)
-      expect(processor.mean_difference).to eql(processor.mean_difference(0.95))
+      expect(@processor.mean_difference).to eql(@processor.mean_difference(0.95))
     end
   end # 'mean intervals'
 
   describe 'variability intervals' do 
     it 'gives interval estimate of difference between two standard deviations' do 
-      processor = DataAnalyst.new(DATASET_1,DATASET_2)
-      expect(processor.sdev_difference).to eql({ :interval => [0.81951, 1.0829], :confidence => 0.95 })
+      expect(@processor.sdev_difference).to eql({ :interval => [0.81951, 1.0829], :confidence => 0.95 })
     end
 
     it 'uses 95% confidence level by default' do 
-      processor = DataAnalyst.new(DATASET_1,DATASET_2)
-      expect(processor.sdev_difference).to eql(processor.sdev_difference(0.95))
+      expect(@processor.sdev_difference).to eql(@processor.sdev_difference(0.95))
     end
   end # 'variability intervals'
 end # describe ConfidenceInterval
