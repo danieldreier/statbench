968e1b9e030853e498e9db3c359fd3a87463606f
Dana Scheider <dana@thinkplango.com>
2013-12-16 13:06:50 -0800

    Added #mean_difference method

--- lib/confidence_interval.rb
+++ lib/confidence_interval.rb
@@ -1,7 +1,9 @@
 require 'statsample'
+require_relative 'test_statistic'
 
 module ConfidenceInterval
   include Statsample
+  include TestStatisticHelper
   
   # The get_variables method is also present in HypothesisTest.
   # When we refactor we will want to extract these to DRY it up.
@@ -14,7 +16,16 @@ module ConfidenceInterval
 
   def mean_difference(hash=@hash)
     get_variables(hash)
-    @mean1 - @mean2
+    @alpha    = 0.05
+    nu        = @nu1 + @nu2
+    mse       = (@var1 + @var2).quo(2)
+    std_error = Math.sqrt((2 * mse) / (nu + 2))
+    t         = TestStatisticHelper::initialize_with(:distribution       => :t,
+                                                     :alpha              => @alpha / 2,
+                                                     :degrees_of_freedom => nu)
+    lower = ((sample_diff = @mean1 - @mean2) - (margin = t.abs * std_error)).round(5)
+    upper = (sample_diff +  margin).round(5)
+    { :interval => [lower,upper], :confidence => 1 - @alpha }
   end
 
   def sdev_difference(hash=@hash)
