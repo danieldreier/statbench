f63f3cfd8b6875131b09f0c28eb7a05f4e700890
Dana Scheider <dana@thinkplango.com>
2013-12-16 12:11:28 -0800

    Changed arguments of #mean_difference() and #sdev_difference() to accept hash from DataAnalyst

--- lib/confidence_interval.rb
+++ lib/confidence_interval.rb
@@ -3,11 +3,22 @@ require 'statsample'
 module ConfidenceInterval
   include Statsample
   
-  def mean_difference(data1=@data1,data2=@data2)
-    data1.mean - data2.mean
+  # The get_variables method is also present in HypothesisTest.
+  # When we refactor we will want to extract these to DRY it up.
+  def get_variables(hash)
+    hash.each do |key,value|
+      name = '@' + key
+      instance_variable_set(name,value)
+    end
   end
 
-  def sdev_difference(data1=@data1,data2=@data2)
-    data1.standard_deviation_sample - data2.standard_deviation_sample
+  def mean_difference(hash=@hash)
+    get_variables(hash)
+    @mean1 - @mean2
+  end
+
+  def sdev_difference(hash=@hash)
+    get_variables(hash)
+    (@sdev1 ||= Math.sqrt(@var1)) - (@sdev2 ||= Math.sqrt(@var2))
   end
 end
