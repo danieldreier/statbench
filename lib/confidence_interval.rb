927bb2a888e018cc5ee3a639c614d05177d31792
Dana Scheider <dana@thinkplango.com>
2013-12-16 11:58:58 -0800

    Changed default values of arguments from @dataset_1 and @dataset_2 to @data1 and @data2, reflecting current instance variable names in the DataAnalyst class

--- lib/confidence_interval.rb
+++ lib/confidence_interval.rb
@@ -3,11 +3,11 @@ require 'statsample'
 module ConfidenceInterval
   include Statsample
   
-  def mean_difference(data1=@dataset_1,data2=@dataset_2)
+  def mean_difference(data1=@data1,data2=@data2)
     data1.mean - data2.mean
   end
 
-  def sdev_difference(data1=@dataset_1,data2=@dataset_2)
+  def sdev_difference(data1=@data1,data2=@data2)
     data1.standard_deviation_sample - data2.standard_deviation_sample
   end
 end
