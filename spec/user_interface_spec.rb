efe883b1999f80ab6382058855636a65ef660872
Dana Scheider <dscheider@t61.local>
2014-01-28 14:24:56 -0800

    Added preliminary list of tests we'll need for the user interface

---
+++ spec/user_interface_spec.rb
@@ -0,0 +1,10 @@
+require_relative 'spec_helper'
+
+describe UserInterface do 
+  it "Takes files as input"
+  it "Returns a confidence interval for change in consistency"
+  it "Returns a confidence interval for change in speed"
+  it "Returns results of a hypothesis test for change in consistency"
+  it "Returns results of a hypothesis test for change in speed"
+  it "Returns summary report of changes in performance"
+end # describe UserInterface
