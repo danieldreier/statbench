d1692ec8b8e5af98905f88ca848c024a74bc6279
Dana Scheider <dscheider@t61.local>
2014-01-31 14:40:17 -0800

    Updated spec to include only specs for Summary up front

--- spec/user_interface_spec.rb
+++ spec/user_interface_spec.rb
@@ -2,9 +2,12 @@ require_relative 'spec_helper'
 
 describe UserInterface do 
   it "Takes files as input"
-  it "Returns a confidence interval for change in consistency"
-  it "Returns a confidence interval for change in speed"
-  it "Returns results of a hypothesis test for change in consistency"
-  it "Returns results of a hypothesis test for change in speed"
-  it "Returns summary report of changes in performance"
+  describe Summary do 
+    it "Tells the user when the new config is faster" 
+    it "Tells the user when the new config is slower"
+    it "Tells the user when there's no change in speed"
+    it "Tells the user when the new config is more consistent"
+    it "Tells the user when the new config is more variable"
+    it "Tells the user when there's no change in variability"
+  end
 end # describe UserInterface
