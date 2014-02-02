aaf0b61d7eeff2bedb22ec84423103c0dbd592d2
Dana Scheider <dscheider@t61.local>
2014-02-02 13:57:29 -0800

    Added a comment to remind myself of a variable scope issue I don't have time to resolve right now.

--- lib/data_analyst.rb
+++ lib/data_analyst.rb
@@ -43,7 +43,26 @@ class DataAnalyst
   end
 
   def summary_stats
-    # TBD
+    stats = {:old_config => nil, :new_config => nil}
+    [@data1,@data2].each do |dataset|
+      # Summary statistics include min, max, median, iqr, upper fence, and lower fence
+      min = dataset.min; max = dataset.max 
+      upper_fence = (median = dataset.median + (fence_range = 1.5 * (iqr = (q1 = dataset.percentil(25)) - (q2 = dataset.percentil(75)))))
+      lower_fence = median - fence_range
+
+      stats.each do |key, value|
+        if value == nil         # Note: We have a variable scope issue here
+          value = { :min => min,
+                    :max => max, 
+                    :median => median, 
+                    :iqr => iqr, 
+                    :upper_fence => upper_fence, 
+                    :lower_fence => lower_fence }
+          break
+        end
+      end
+    end
+    stats
   end
 end
 
