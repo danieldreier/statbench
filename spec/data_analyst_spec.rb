35fadc6278f1813eb8010e5bbb75c604e22eb027
Dana Scheider <dscheider@t61.local>
2014-02-01 19:28:49 -0800

    Changed name of ::process method to ::process_data to contrast with newly-to-be-added ::process_file method

--- spec/data_analyst_spec.rb
+++ spec/data_analyst_spec.rb
@@ -5,7 +5,7 @@ describe DataAnalyst do
     processor = DataAnalyst.new(DATA_FILE_1,DATA_FILE_2)
     expected_result = { 'nu1' => 199, 'mean1' => 28.6537, 'var1' => 279.2421,
                         'nu2' => 199, 'mean2' => 29.2633, 'var2' => 314.6817 }
-    expect(processor.process).to eql(expected_result)
+    expect(processor.process_data).to eql(expected_result)
   end
 
   it 'initializes with a processed hash' do 
