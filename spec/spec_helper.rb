e902e627aebccc30e9c17f5c3ddc9c7b7599a966
Dana Scheider <dscheider@t61.local>
2014-02-01 17:28:22 -0800

    Removed requirements for data files; all of them will be required in spec/data.rb instead

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,12 +3,4 @@ require_relative '../lib/hypothesis_test.rb'
 require_relative '../lib/confidence_interval.rb'
 require_relative '../lib/test_statistic.rb'
 require_relative '../lib/test_statistic_helper.rb'
-
-# Test data
 require_relative 'data'
-require_relative 'data_files/data1.txt'
-require_relative 'data_files/data2.txt'
-require_relative 'data_files/data3.txt'
-require_relative 'data_files/data4.txt'
-require_relative 'data_files/data5.txt'
-require_relative 'data_files/data6.txt'
