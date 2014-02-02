cbd0d89bec17c9aa0decfee66e5d19184db8de86
Dana Scheider <dscheider@t61.local>
2014-02-01 17:50:43 -0800

    Updated specs to require DataAnalyst to take files as input

--- spec/data_analyst_spec.rb
+++ spec/data_analyst_spec.rb
@@ -1,15 +1,15 @@
 require_relative 'spec_helper'
 
 describe DataAnalyst do 
-  it 'gets statistics from data sets' do 
-    processor = DataAnalyst.new(DATASET_1,DATASET_2)
+  it 'gets statistics from data files' do 
+    processor = DataAnalyst.new(DATA_FILE_1,DATA_FILE_2)
     expected_result = { 'nu1' => 199, 'mean1' => 28.6537, 'var1' => 279.2421,
                         'nu2' => 199, 'mean2' => 29.2633, 'var2' => 314.6817 }
     expect(processor.process).to eql(expected_result)
   end
 
   it 'initializes with a processed hash' do 
-    processor = DataAnalyst.new(DATASET_1,DATASET_2)
+    processor = DataAnalyst.new(DATA_FILE_1,DATA_FILE_2)
     expect(processor.hash).to eql( { 'nu1' => 199, 'mean1' => 28.6537, 'var1' => 279.2421,
                                      'nu2' => 199, 'mean2' => 29.2633, 'var2' => 314.6817 })
   end
