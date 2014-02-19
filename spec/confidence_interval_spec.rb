e47a3a7a005c701146a0550744501f88d80ee967
Dana Scheider <dana.scheider@gmail.com>
2014-02-19 13:55:37 -0800

    Add DataAnalyst instance handling files

--- spec/confidence_interval_spec.rb
+++ spec/confidence_interval_spec.rb
@@ -3,34 +3,35 @@ require_relative 'spec_helper'
 describe ConfidenceInterval do 
   before(:all) do 
     @processor = DataAnalyst.new(DATASET_1,DATASET_2)
+    @file_processor = DataAnalyst.new(DATA_FILE_1,DATA_FILE_2)
   end
 
   describe 'mean intervals' do 
     it 'gives interval estimate of difference between two means' do 
-      expect(@processor.mean_difference).to eql({ :interval => [-3.00515, 1.78595], :confidence => 0.95})
+      expect(@file_processor.mean_difference).to eql({ :interval => [-3.00515, 1.78595], :confidence => 0.95})
     end
 
     it 'uses 95% confidence level by default' do 
-      expect(@processor.mean_difference).to eql(@processor.mean_difference(@processor.hash,0.95))
+      expect(file_@processor.mean_difference).to eql(@file_processor.mean_difference(@file_processor.hash,0.95))
     end
 
     it 'allows user to specify confidence level' do 
-      expect(@processor.mean_difference(@processor.hash,0.99)).to eql({ :interval => [ -3.76344, 2.54424 ], 
+      expect(@file_processor.mean_difference(@file_processor.hash,0.99)).to eql({ :interval => [ -3.76344, 2.54424 ], 
                                                                         :confidence => 0.99})
     end
   end # 'mean intervals'
 
   describe 'variability intervals' do 
     it 'gives interval estimate of difference between two standard deviations' do 
-      expect(@processor.sdev_difference).to eql({ :interval => [0.81951, 1.0829], :confidence => 0.95 })
+      expect(@file_processor.sdev_difference).to eql({ :interval => [0.81951, 1.0829], :confidence => 0.95 })
     end
 
     it 'uses 95% confidence level by default' do 
-      expect(@processor.sdev_difference).to eql(@processor.sdev_difference(@processor.hash,0.95))
+      expect(@file_processor.sdev_difference).to eql(@file_processor.sdev_difference(@file_processor.hash,0.95))
     end
 
     it 'allows user to specify confidence level' do 
-      expect(@processor.sdev_difference(@processor.hash, 0.99)).to eql( { :interval => [ 0.78421, 1.13158 ], :confidence => 0.99 })
+      expect(@file_processor.sdev_difference(@file_processor.hash, 0.99)).to eql( { :interval => [ 0.78421, 1.13158 ], :confidence => 0.99 })
     end
   end # 'variability intervals'
 end # describe ConfidenceInterval
