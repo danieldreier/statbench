130805d901bba1b4ca184fda54e3e14e9e2833dc
Dana Scheider <dana.scheider@gmail.com>
2014-02-20 15:49:32 -0800

    Added complexity to troubleshooting files - included them in spec_helper

--- lib/troubleshooting.rb
+++ lib/troubleshooting.rb
@@ -1,2 +1,19 @@
-module Troubleshooting
+require 'statsample'
+require_relative 'confidence_interval'
+require_relative 'hypothesis_test'
+
+class Troubleshooter
+  include Statsample
+  include ConfidenceInterval
+  include HypothesisTest
+
+  attr_reader :data1
+  attr_reader :data2
+
+  def initialize(data1,data2)
+    # assume data sets are definitely strings for now
+    @data1 = Array.new; @data2 = Array.new
+    File.open(data1,'r+') { |file| file.each_line { |line| @data1 << line.chomp } }
+    File.open(data2,'r+') { |file| file.each_line { |line|@data2 << line.chomp } }
+  end
 end
--- spec/troubleshooting_spec.rb
+++ spec/troubleshooting_spec.rb
@@ -1,7 +1,11 @@
 require_relative 'spec_helper'
 
-describe DataAnalyst do 
+describe Troubleshooter do 
   before(:all) do 
-    processor = DataAnalyst.new(DATASET_1,DATASET_2)
+    @processor = Troubleshooter.new(DATA_FILE_1,DATA_FILE_2)
+  end
+
+  it "can run without fucking up" do 
+    expect(@processor.data1.class).to eql(Array)
   end
 end
