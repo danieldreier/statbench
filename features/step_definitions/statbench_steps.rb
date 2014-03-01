f66cde60dc3106ebf6e7b53475916a854f0ba78e
Dana Scheider <dana.scheider@gmail.com>
2014-03-01 12:00:53 -0800

    Steps for statbench.feature

---
+++ features/step_definitions/statbench_steps.rb
@@ -0,0 +1,15 @@
+Given(/^two data files$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I request summary data$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should be told which configuration is faster$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should be told which configuration is more consistent$/) do
+  pending # express the regexp above with the code you wish you had
+end
