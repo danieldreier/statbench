4d8a11f13dcb5de9605f68a36d52153f11547c81
Dana Scheider <dana.scheider@gmail.com>
2014-03-02 15:13:53 -0800

    Delete definitions already present in aruba library

--- features/step_definitions/statbench_steps.rb
+++ features/step_definitions/statbench_steps.rb
@@ -1,16 +1,8 @@
-Given(/^two data files$/) do
+Given(/^two data sets$/) do
   @data1 = "#{ENV['PWD']}/spec/test_data/data1.txt"
   @data2 = "#{ENV['PWD']}/spec/test_data/data2.txt"
 end
 
 When(/^I request a comparison$/) do
   `bundle exec bin/statbench --old #{@data1} --new #{@data2} compare`
-end
-
-Then(/^I should be told which configuration is faster$/) do
-  puts "Average response time hasn't changed."
-end
-
-Then(/^I should be told which configuration is more consistent$/) do
-  puts "Variability in response times hasn't changed."
 end
