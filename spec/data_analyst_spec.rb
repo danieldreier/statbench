adcbae8e79bc75e9a704618f6ad70019cfce8b59
Dana Scheider <dana.scheider@gmail.com>
2014-03-02 16:06:34 -0800

    Add pending tests for controller functionalities

--- spec/data_analyst_spec.rb
+++ spec/data_analyst_spec.rb
@@ -17,21 +17,7 @@ describe DataAnalyst do
                                      'nu2' => 199, 'mean2' => 29.2633, 'var2' => 314.6817 })
   end
 
-  it 'returns summary statistics' do 
-    expect(@processor.summary_stats).to eql( {:old_config=> { :min=>0.34, 
-                                                            :max=>58.71, 
-                                                            :median=>29.355, 
-                                                            :iqr=>42.8, 
-                                                            :upper_fence=>78.965, 
-                                                            :lower_fence=>-34.845 
-                                                          }, 
-                                            :new_config=> { :min=>0.01, 
-                                                            :max=>59.98, 
-                                                            :median=>29.515, 
-                                                            :iqr=>44.78, 
-                                                            :upper_fence=>82.86, 
-                                                            :lower_fence=>-37.655
-                                                          }
-                                            })
-  end
+  it 'gets a confidence interval from the ConfidenceInterval module'
+  it 'gets test results from the HypothesisTest module'
+  it 'gets summary statistics from the SummaryStats module'
 end
